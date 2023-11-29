eb9e6860abbd0a39775592aadcbccc16de8c5b0b
Amidu99 <amidushamika@gmail.com>
2023-11-29 19:38:26 +0530

    complete the scanning

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const html5QrCode = new Html5Qrcode("reader");
+    function onScanSuccess(decodedText, decodedResult) {
+        console.log(`Code matched = ${decodedText}`, decodedResult);
+        alert(`Code matched = ${decodedText}`);
+    }
+function onScanFailure(error) {
+    console.warn(`Code scan error = ${error}`);
+}
+// Start scanning
+html5QrCode.start(
+{ facingMode: "environment" },
+{ fps: 10, qrbox: { width: 250, height: 250 } },
+    onScanSuccess,
+    onScanFailure
+    ).catch((err) => {
+    console.error(`Start failed. Reason: ${err}`);
+});
+// Stop scanning when the window is closed
+window.addEventListener("beforeunload", function () {
+    if (html5QrCode) {
+    html5QrCode.stop().then(() => {
+        console.log("QR Code scanning stopped");
+    }).catch((err) => {
+        console.error(`Stop failed. Reason: ${err}`);
+    });
+    }
+});
+});
